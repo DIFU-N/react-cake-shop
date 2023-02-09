9583dacf3f569e776429fa915959aa218cf8f89b
DIFU-N <chukwudifunsofor@gmail.com>
2023-02-09 11:33:17 +0100

    api call result showing in page

--- src/features/user/userView.jsx
+++ src/features/user/userView.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUsers } from './user-slice'
 
 export const UserView = () => {
+  const usersRetrieved = useSelector((state) => state.user)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
   return (
-    <div>List of Users</div>
+    <div>
+      <h2>List of Users</h2>
+      {usersRetrieved.loading && <div>Loading...</div>}
+      {!usersRetrieved.loading && usersRetrieved.error ? <div>Error: {usersRetrieved.error}</div> : null}
+      {console.log(usersRetrieved.users)}
+      {!usersRetrieved.loading && usersRetrieved.users.length ? (
+        <ul>
+          {
+            usersRetrieved.users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))
+          }
+        </ul>
+      ) : null}
+    </div>
   )
 }
